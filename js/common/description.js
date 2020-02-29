913a8aef61f843b8a639d1274f1a00a7dbc0f128
Jinyang-Guo <61546915+Jinyang-Guo@users.noreply.github.com>
2020-02-29 18:59:45 +0800

    change

---
+++ js/common/description.js
@@ -0,0 +1,11 @@
+document.writeln("<meta charset=\'utf-8\'>");
+document.writeln("    <meta name=\'viewport\' content=\'width=device-width, initial-scale=1.0\'>");
+document.writeln("    <meta name=\'description\' content=\'Developed By JinyangGuo\'>");
+document.writeln("    <meta name=\'author\' content=\'cosmic\'>");
+document.writeln("    <meta name=\'keywords\' content=\'APPT, parallel architectures, hardware and software systems, distributed and cloud computing, parallel algorithms, conference, CCF, operating system\'>");
+document.writeln("    <meta http-equiv=\'pragma\' content=\'no-cache\'> ");
+document.writeln("    <meta http-equiv=\'Cache-Control\' content=\'no-cache, no-store, must-revalidate\'> ");
+document.writeln("    <meta http-equiv=\'expires\' content=\'Wed, 26 Feb 1997 08:21:57 GMT\'>");
+document.writeln("    <link rel=\'shortcut icon\' href=\'img/tag_icon.png\'>");
+document.writeln("");
+document.writeln("    <title>APPT 2021| SP </title>");
